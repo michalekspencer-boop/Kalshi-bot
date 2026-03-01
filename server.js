90e6f3393fb277e109b7d2e0b812e7f38a457eb8
michalekspencer-boop <michalekspencer@gmail.com>
2026-03-01 13:15:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const KALSHI_BASE = "https://api.kalshi.com/trade-api/v2";
+const KALSHI_BASE = "https://api.elections.kalshi.com/trade-api/v2";
 let token = null;
 
 // Login to Kalshi
